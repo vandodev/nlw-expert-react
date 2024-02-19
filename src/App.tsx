f7548e6b57ab9245370dd724509563b7e32e82d9
vandodev <vandoaparecido@hotmail.com>
2024-02-19 08:27:16 -0300

    :notebook: :bookmark_tabs: :clipboard: Criando Dialog com Radix-ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@ import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
-const note = {
-  date: new Date(),
-  content: "olá mundo"
-}
-
 export function App() {
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
@@ -24,12 +19,12 @@ export function App() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         
-        <NewNoteCard />
-      
-        {/* <NoteCard date={new Date()} content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ea doloribus repellat autem, eius, repudiandae vitae architecto error aspernatur soluta distinctio voluptatum dolorem labore. Maiores placeat velit suscipit ducimus quaerat!"/>
-        <NoteCard date={new Date(23, 4, 1)} content=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ea doloribus repellat autem, eius, repudiandae vitae architecto error aspernatur soluta distinctio voluptatum dolorem labore. Maiores placeat velit suscipit ducimus quaerat!"/> */}
-
-        <NoteCard note={note}/>
+        <NewNoteCard />      
+        
+        <NoteCard note={{
+            date: new Date(),
+            content: "olá mundo"
+        }}/>
 
       </div>
     </div>
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,26 +1,36 @@
-interface NoteCardProps {    
-    // date: Date;
-    // content: string;    
+import * as Dialog from "@radix-ui/react-dialog";
+
+interface NoteCardProps {  
     note: {
         date: Date;
         content: string;
     };
   }
-//   export function NoteCard(props:NoteCardProps){
+
 export function NoteCard({note}:NoteCardProps){
     return(
-        <button className="rounded-md flex flex-col gap-3 text-left relative overflow-hidden bg-slate-800 p-5 hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
-            
-            <span className="text-sm font-medium text-slate-300">
-                {/* há {props.note.date.toISOString()} dias */}
-                há {note.date.toISOString()} dias
-            </span>
+        <Dialog.Root>
+            <Dialog.Trigger className="rounded-md flex flex-col gap-3 text-left relative overflow-hidden bg-slate-800 p-5 hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
+                
+                <span className="text-sm font-medium text-slate-300">
+                    {/* há {props.note.date.toISOString()} dias */}
+                    há {note.date.toISOString()} dias
+                </span>
 
-            <p className="text-sm leading-6 text-slate-400">
-                {note.content}
-            </p>
+                <p className="text-sm leading-6 text-slate-400">
+                    {note.content}
+                </p>
 
-            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
-        </button>
+                <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"></div>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+                <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+                <Dialog.Content className="fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none">
+                    oi
+                </Dialog.Content>
+            </Dialog.Portal>
+
+        </Dialog.Root>
     )
 }
