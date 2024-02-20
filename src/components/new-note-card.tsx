ae9f830403f2176f65cc0990d29ba435badae55a
vandodev <vandoaparecido@hotmail.com>
2024-02-20 07:57:20 -0300

    :notebook: :bookmark_tabs: :clipboard: Mostrando onbording condicional

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,7 +1,14 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
+import { useState } from "react";
 
 export function NewNoteCard(){
+    const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
+
+    function handleStartEditor() {
+        setShouldShowOnboarding(false);
+    }
+
     return(
         <Dialog.Root>
             <Dialog.Trigger className="rounded-md flex flex-col gap-3 text-left bg-slate-700 p-5 hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
@@ -23,38 +30,44 @@ export function NewNoteCard(){
                 </Dialog.Close>
     
                 <form className="flex-1 flex flex-col">
-                <div className="flex flex-1 flex-col gap-3 p-5">
-                    <span className="text-sm font-medium text-slate-300">
-                    Adicionar nota
-                    </span>  
+                    <div className="flex flex-1 flex-col gap-3 p-5">
+                        <span className="text-sm font-medium text-slate-300">
+                        Adicionar nota
+                        </span>  
 
-                    <p className="text-sm leading-6 text-slate-400">
-                        Comece{" "}
-                        <button
-                            type="button"
-                            className="font-medium text-lime-400 hover:underline"
-                        >
-                            gravando uma nota
-                        </button>{" "}
-                        em áudio ou se preferir{" "}
-                        <button
-                            type="button"
-                            className="font-medium text-lime-400 hover:underline"
-                        >
-                            utilize apenas texto
-                        </button>
-                        .
-                    </p>
+                        {shouldShowOnboarding ? (
+                            <p className="text-sm leading-6 text-slate-400">
+                                Comece{" "}
+                                <button
+                                    type="button"
+                                    className="font-medium text-lime-400 hover:underline"
+                                >
+                                    gravando uma nota
+                                </button>{" "}
+                                em áudio ou se preferir{" "}
+                                <button
+                                    type="button"
+                                    className="font-medium text-lime-400 hover:underline"
+                                    onClick={handleStartEditor}
+                                >
+                                    utilize apenas texto
+                                </button>
+                                .
+                            </p>
 
-                </div>   
+                        ):(
+                            <p>Editor</p>
+                        )}
 
 
-                <button
-                    type="button"
-                    className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
-                >
-                    Salvar nota
-                </button>         
+                    </div>   
+
+                    <button
+                        type="button"
+                        className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+                    >
+                        Salvar nota
+                    </button>         
                 </form>
 
             </Dialog.Content>
