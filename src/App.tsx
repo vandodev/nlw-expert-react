b7dedc3221d000bbc269cc3c1f741165dc925bc2
vandodev <vandoaparecido@hotmail.com>
2024-02-21 07:36:12 -0300

    :notebook: :bookmark_tabs: :clipboard: Salvando nota no estado

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,16 @@ export function App() {
     {id:2, date: new Date(), content: "Nota 2"}
   ])
 
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id:Math.random(),
+      date: new Date(),
+      content,
+    }
+
+    setNotes([newNote, ...notes])
+  }
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="NLW Expert" />
@@ -25,7 +35,7 @@ export function App() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
         
-        <NewNoteCard />      
+        <NewNoteCard onNoteCreated={onNoteCreated}/>      
         
       
         {notes.map(note => {
--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,7 +3,11 @@ import { X } from "lucide-react";
 import {ChangeEvent,  FormEvent,  useState } from "react";
 import { toast } from "sonner";
 
-export function NewNoteCard(){
+interface NewNoteProps {
+    onNoteCreated: (content: string) => void;
+}
+
+export function NewNoteCard({onNoteCreated}: NewNoteProps){
     const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
     const [content, setContent] = useState("");
 
@@ -23,7 +27,9 @@ export function NewNoteCard(){
     
         if (content === "") {
           return;
-        }        
+        }
+        
+        onNoteCreated(content)
     
         setContent("");
         setShouldShowOnboarding(true);
