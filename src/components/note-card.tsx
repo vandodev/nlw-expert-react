c018b83e9945472a7d2caf222848427462a33923
vandodev <vandoaparecido@hotmail.com>
2024-02-23 07:40:31 -0300

    :notebook: :bookmark_tabs: :clipboard: Deletando nota

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -5,12 +5,14 @@ import { ptBR } from "date-fns/locale";
 
 interface NoteCardProps {  
     note: {
+        id: string;
         date: Date;
         content: string;
     };
+    onNoteDeleted: (id: string) => void;
   }
 
-export function NoteCard({note}:NoteCardProps){
+export function NoteCard({note, onNoteDeleted}:NoteCardProps){
     return(
         <Dialog.Root>
             <Dialog.Trigger className="rounded-md flex flex-col gap-3 text-left relative overflow-hidden bg-slate-800 p-5 hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
@@ -51,7 +53,8 @@ export function NoteCard({note}:NoteCardProps){
                     <button
                         type="button"
                         className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
-                    >
+                        onClick={() => onNoteDeleted(note.id)}
+                   >
                         Deseja{" "}
                         <span className="text-red-400 group-hover:underline">
                         apagar essa nota
